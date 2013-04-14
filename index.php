486b68e454ebf7bd9cbeb30db8a2774a3e96d6e1
nilsding <nilsding@myopera.com>
2013-04-14 19:55:45 +0200

    Link zu diesem Github hinzugefügt.

--- index.php
+++ index.php
@@ -18,5 +18,7 @@ require_once('config.php');
 <h1>nilsdinghatlimit</h1>
 <p>Das hier ist ein Service, den ich verwenden werde, um euch über eure Accounts zu benachrichtigen, dass ich Limit habe.</p>
 <p><?php if (isset($_SESSION['status']) && $_SESSION['status'] == 'verified') echo 'Vielen Dank!'; else echo '<a href="signin.php">Sich mit Twitter anmelden.</a>' ?></p>
+<hr />
+<p style="font-size: small;"><a href="https://github.com/nilsding/nilsdinghatlimit">Sie können diese auch selber hosten! Oder dazu beitragen.</a></p>
 </body>
 </html>
