72c19328a9489ec21dfcb6198c7fac0347c767a7
Stephan Krinetzki <therealkings@gmail.com>
2013-05-01 22:34:37 +0200

    No duplicated users anymore
    To ensure that no user is duplicated in the database.
    Some MySQL optimizing (or so...)

--- callback.php
+++ callback.php
@@ -23,10 +23,32 @@ unset($_SESSION['oauth_token']);
 unset($_SESSION['oauth_token_secret']);
 
 if (200 == $connection->http_code) {
-	$_SESSION['status'] = 'verified';
-	$sql = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
-	$sqls =  "INSERT INTO ndl_users (user_id, access_token, access_token_secret) VALUES (" . $access_token['user_id']. ", '" . $access_token['oauth_token'] . "', '" . $access_token['oauth_token_secret'] . "');";
-	$q = mysqli_query($sql, $sqls);
+	$_SESSION['status'] = 'verified';	
+	
+	/* To ensure that no user is duplicated in the database */
+	/* And some MySQL optimizing							*/
+	/* Edited by therealkings								*/
+	
+	$mysqli = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+	
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+
+	$result = $mysqli->query("SELECT user_id from ndl_users WHERE `user_id` = '".$access_token['user_id']."'");
+		if($result->num_rows > 0)
+			{
+				$sqls = "UPDATE SET `access_token` = '".$access_token['oauth_token']."', `access_token_secret` = '".$access_token['oauth_token_secret']."' WHERE `user_id` = '".$access_token['user_id']."'";
+			}
+		else
+			{
+				$sqls =  "INSERT INTO ndl_users (user_id, access_token, access_token_secret) VALUES (" . $access_token['user_id']. ", '" . $access_token['oauth_token'] . "', '" . $access_token['oauth_token_secret'] . "');";
+			}
+	
+	$q = $mysqli->query($sqls);
+	/* End edit 											*/
+	
 	header('Location: ./index.php');
 } else {
 	header('Location: ./clearsessions.php');
